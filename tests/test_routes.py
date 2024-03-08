5955e9c601701a9859778cdc3d1f53821d72c801
edwinsibarra <124415700+edwinsibarra@users.noreply.github.com>
2024-03-08 16:27:11 -0600

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -18,7 +18,7 @@ DATABASE_URI = os.getenv(
 )
 
 BASE_URL = "/accounts"
-
+HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
 ######################################################################
 #  T E S T   C A S E S
@@ -34,6 +34,7 @@ class TestAccountService(TestCase):
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
+        talisman.force_https = False
 
     @classmethod
     def tearDownClass(cls):
